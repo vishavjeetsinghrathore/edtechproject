d0663565025f8546d30f6add4f990f081cfedb7e
vishavjeetsinghrathore <2021kucp1030@iiitkota.ac.in>
2024-02-07 23:27:17 +0530

    codeblock updated

--- src/components/core/HomePage/CodeBlocks.js
+++ src/components/core/HomePage/CodeBlocks.js
@@ -7,16 +7,16 @@ import CTAButton from "./Button"
 import { TypeAnimation } from 'react-type-animation'
 
 const CodeBlocks=({
-    position,heading,subheading,ctabtn1,ctabtn2,codeblock,backgroundGradient,codeColor
+    position,heading,subheading,ctabtn1,ctabtn2,codeblock,codeColor,backgroundGradient
 })=>{
 
      return(
-        <div className={`flex ${position} my-20 justify-between gap-10`}>
+        <div className={`flex ${position} my-20 justify-between flex-col lg:gap-10 gap-10`}>
             {/*section1*/}
 
-        <div className='w-[50%] flex flex-col gap-8'>
+        <div className="w-[100%] lg:w-[50%] flex flex-col gap-8">
             {heading}
-            <div className="text-richblack-300 font-bold">
+            <div className="text-richblack-300 text-base font-bold w-[85%] -mt-3">
                 {subheading}
             </div>
             <div className="flex gap-7 mt-7">
@@ -35,8 +35,9 @@ const CodeBlocks=({
         </div>
 
         {/*section2*/}
-        <div className="h-fit flex flex-row text-10[px] w-[100%] py-4 lg:w-[500px]">
-            <div className="text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold">
+        <div className="h-fit code-border flex flex-row py-3 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-[100%] lg:w-[470px]">
+        {backgroundGradient}
+            <div className="text-center flex flex-col   w-[10%] select-none text-richblack-400 font-inter font-bold ">
                 <p>1</p>
                 <p>2</p>
                 <p>3</p>
@@ -49,7 +50,7 @@ const CodeBlocks=({
                 <p>10</p>
                 <p>11</p>
             </div>
-            <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono pr-2 ${codeColor} pr-2`}>
+            <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-1`}>
                 <TypeAnimation
 
                 sequence={[codeblock,10000,""]}
